226317a25597608330e52408f38e04ad68d11292
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-08-08 22:14:00 -0400

    Secure DB info in an .env file

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
 
-const connection = await mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'YOUR MYSQL PASSWORD',
-	database: 'forum'
+dotenv.config();
+
+const connection = mysql.createConnection({
+	"host": process.env.DB_HOST,
+	"user": process.env.DB_USER,
+	"password": process.env.DB_PASSWORD,
+	"database": process.env.DB_NAME
 })
 
 export default connection;
