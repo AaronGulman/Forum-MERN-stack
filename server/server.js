f1e29ddd360d406c45ba00c10352bbd7f9785b32
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-08-11 22:22:00 -0400

    Add user registration template with bcrypt password hashing

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import express from "express"
 import cors from 'cors'
+import bcrypt from 'bcrypt';
 import connection from './db/connection.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 
 
 const server = express();
 const PORT = process.env.PORT || 3000
+const saltRounds = 10;
 
 server.use(cors());
 server.use(express.json())
@@ -12,14 +18,44 @@ server.use(express.json())
 
 server.get('/test-db', async (req, res) => {
     try {
-        const [rows] = await connection.query('SELECT 1 + 1 AS result');
+        const [rows] = await connection.query('SELECT * FROM users');
+        console.log(req.body)
         res.json(rows);
     } catch (error) {
         res.status(500).json({ error: 'Database connection failed' });
     }
 });
 
-server.listen(PORT ,async (req,res)=>{
+server.post('/test-db', async (req,res)=> {
+    try{
+        const {username,email,password} = req.body;
+        if(!username || !password || !email){
+           return res.status(400).json({error:"Username,email and password are required"});
+        }
+        const [existingUser] = await connection.query(
+            'SELECT * FROM users WHERE username = ?',
+            [username]
+        );
 
+        if (existingUser.length > 0) {
+            return res.status(400).json({ error: 'Username already exists' });
+        }
+
+        //Here's our password hashing:
+        const password_hash = await bcrypt.hash(password,saltRounds);
+
+        const [result] = connection.query(
+            `INSERT INTO users (username,email,password_hash) VALUES (?,?,?)`,
+            [username,email,password_hash]
+        );
+
+        res.status(201).json({id: result.insertedId,username,email})
+
+    }catch(error){
+        res.status(500).json({error: 'Database connection failed'})
+    }
+})
+
+server.listen(PORT ,async (req,res)=>{
 	console.log(`Running on ${PORT}`)
 })
