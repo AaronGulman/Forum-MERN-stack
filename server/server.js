56b4fea20e1b583c5bc8f018991f39682375a5a5
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-08-21 21:37:15 -0400

    feat(server): Add cookie-parser middleware to the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import users from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,8 +11,10 @@ const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser())
 
-server.use("/", users);
+server.use("/", users);x
+
 
 server.listen(PORT, async (req, res) => {
   console.log(`Running on ${PORT}`);
