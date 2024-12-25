94c47fb073a49e198daf0903d5ecabf4c18a127c
AaronGulman <enggulman@gmail.com>
2024-12-24 22:32:23 -0500

    Add initial styling in the tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f3f4f6',
+        secondary: '#f9fafb',
+        accent: '#f87171',
+        dark: '#1a202c',
+        light: '#e2e8f0',
+        black: '#000000',
+        white: '#ffffff',
+      },
+      
+    },
   },
   plugins: [],
 }
