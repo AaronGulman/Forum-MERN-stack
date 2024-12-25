e5b4c058a0b7f7a988c610ecf2483d933fbadc00
AaronGulman <enggulman@gmail.com>
2024-12-24 22:33:57 -0500

    Restructure the project and move the components to their dedicated directory

---
+++ client/src/components/auth/SignIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div className='relative flex h-7 bg-blue-500'>
+      <form action="">
+      <input type="text" name='signin' className='text-black'/>
+      <input type="password" name='password' className='text-black'/>
+      <button className='text-black' type="submit">Login</button>
+      
+      </form>
+    </div>
+  )
+}
+
+export default SignIn
--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import SignUp from './SignUp'
-
-
-export {
-	SignUp,
-}
