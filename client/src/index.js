43655a367440f991ab71ac2a91b493f76bef1ec8
AaronGulman <enggulman@gmail.com>
2024-12-25 23:07:46 -0500

    Add index.js for simplified exports and imports

---
+++ client/src/index.js
@@ -0,0 +1,42 @@
+//AUTH
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+
+//Forum
+import CreatePost from './components/forum/CreatePost';
+import PostCard from './components/forum/PostCard';
+import PostList from './components/forum/PostList';
+
+//Layout
+import Navbar from './components/layout/Navbar';
+import Sidebar from './components/layout/Sidebar';
+import Footer from './components/layout/Footer';
+
+//Context
+import AuthContext from './context/AuthContext'; 
+
+//Hooks
+
+//Pages
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+
+//Services
+
+//Utilscccc
+
+export{
+	SignIn,
+	SignUp,
+	CreatePost,
+	PostCard,
+	PostList,
+	Navbar,
+	Sidebar,
+	Footer,
+	AuthContext,
+	Home,
+	Profile,
+	Settings,
+}
