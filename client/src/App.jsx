9c1c16d0f26c96dc1e6b8b7977697ff9d354161c
AaronGulman <enggulman@gmail.com>
2024-12-25 23:07:23 -0500

    Edit App.jsx  : add a few routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { SignUp } from './components';
+import { SignIn, Navbar, SignUp } from './';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -6,9 +6,19 @@ function App() {
 
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="register" element={<SignUp/>}/> 
-    </Routes>
+    <div className='relative z-0 bg-primary'>
+    <div className='bg-black bg-cover '>
+    <Navbar/>
+    </div>
+    </div>
+
+    <Routes path="/SignIn" element={<SignIn/>}/>
+    <Routes path="/SignUp" element={<SignUp/>}/>
+
+   { /* <Routes>
+    <Route path="/register" element={<SignUp/>}/> 
+    </Routes> */}
+    
     </BrowserRouter>
 
   );
